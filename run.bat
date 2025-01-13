bad9b5494492956368a16a4f23ca61fbeca87cca
KeziaWahome <k.wahome@alustudent.com>
2025-01-13 11:27:51 +0300

    updating the .bat file to include the virt evn

--- run.bat
+++ run.bat
@@ -1,4 +1,12 @@
 @echo off
+rem Change to the script directory
 cd C:\Users\Hp\Desktop\Daily_Commit_Script
+
+rem Activate the virtual environment
+call C:\Users\Hp\Desktop\Daily_Commit_Script\DailyCommit\Scripts\activate.bat
+
+rem Run the Python script
 python main.py
+
+rem Pause to keep the window open (optional)
 pause
